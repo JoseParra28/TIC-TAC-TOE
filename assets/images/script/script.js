4ce5880b82261f4c21afc9017d0f31b3ce475619
JoseParra28 <iliftandcook@gmail.com>
2022-10-07 19:47:26 +0000

    add js event listener

--- assets/images/script/script.js
+++ assets/images/script/script.js
@@ -1,3 +1,13 @@
+ 
+ const questionContainerElement = document.getElementById
+ ('question-container')
+ const startButton = document.getElementById ('start-btn')
+ startButton.addEventListener('click', startGame)
+ console.log('Started')
+ startButton.classList.add ('hide')
+ questionContainerElement.classList.remove ('hide')
+ setNextQuestion()
+
  function startGame() {
 
  }
@@ -5,5 +15,15 @@
 
  }
  function selectAnswer () {
-    
+
  }
+const question = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '22', correct: false},
+        ] 
+
+    }
+] 
