a5fdf1894eb415dd8c66271bf5e00dc555f243fe
JoseParra28 <iliftandcook@gmail.com>
2023-01-13 06:44:22 +0000

    add

--- assets/images/script/script.js
+++ assets/images/script/script.js
@@ -1,141 +1,72 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
 
-
-let shuffledQuestions, currentQuestionIndex
-
-let timeleft = 90;
-let downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished!!!";
-  } else {
-    document.getElementById("countdown").innerHTML = timeleft + "Seconds remaining";
-  }
-  timeleft -= 1;
-}, 1000);
-
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-startButton.addEventListener('click', startGame)
-
-function setVisibility(id,Visibility){
-  document.getElementById(id).style.display=Visibility
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
+function boxClicked(e) {
+    const id = e.target.id
 
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
 }
 
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-const questions = [
-  
-  {
-    question: 'What kind of icing is typically used to assemble gingerbread houses?',
-    answers: [
-      { text: 'Italian Buttercream', correct: false },
-      { text: 'Royal Icing', correct: true },
-    ]
-  },
-  {
-    question: 'What kind of cheese is traditionally used in making Tiramisu?',
-    answers: [
-      { text: 'Mascarpone', correct: true },
-      { text: 'Blue cheese', correct: false },
-      
-    ]
-  },
-  {
-    question: ' What two flavors make up "Gianduja?"?',
-    answers: [
-      { text: 'Chocolate & Hazelnut', correct: true },
-      { text: 'Chocolate & Coconut', correct: false }
-    ]
-  },
-  {
-    question: 'What is the French term for the dough used to make eclairs?',
-    answers: [
-      
-      { text: 'Mise en place', correct: false },
-      { text: 'Pate a choux', correct: true }
-    ]
-  },
-  {
-    question: 'Julia Child was born in Paris',
-    answers: [
-      { text: 'True', correct: false },
-      { text: 'False', correct: true }
-    ]
-  },
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ]
 
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
 
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false
+}
 
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach( box => {
+        box.innerText = ''
+        box.style.backgroundColor=''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_TEXT
+}
+
+startGame()
